7fc0025ceba09cce63589eddc02103a8e259d9bf
kangkyeongseo <kks_big@naver.com>
2023-11-15 16:15:45 +0900

    feat: framer motion variants

--- src/App.js
+++ src/App.js
@@ -12,19 +12,51 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: #fff;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
   border-radius: 50px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const Circle = styled(motion.div)`
+  width: 70px;
+  height: 70px;
+  place-self: center;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 50%;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  start: { opacity: 1, scale: 0 },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVariants = {
+  start: { opacity: 0, y: 20 },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box
-        transition={{ type: "spring", bounce: 0.5, delay: 1 }}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-      />
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
