d161da8c7952c781c4d178079dc3df5d7641493e
kangkyeongseo <kks_big@naver.com>
2023-11-15 17:58:17 +0900

    feat: framer motion motionValues

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import {
+  motion,
+  useMotionValue,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+import { useEffect, useRef } from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   width: 100vw;
-  height: 100vh;
+  height: 200vh;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-  border-radius: 50px;
-  background-color: rgba(255, 255, 255, 0.4);
 `;
 
 const Box = styled(motion.div)`
@@ -35,21 +30,28 @@ const boxVariants = {
   drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 10 } },
 };
 function App() {
-  const biggerBoxRef = useRef(null);
+  const x = useMotionValue(0);
+  const { scrollY, scrollYProgress } = useScroll();
+  const rotate = useTransform(x, [-800, 800], [360, -360]);
+  const gradient = useTransform(
+    x,
+    [-800, 0, 800],
+    [
+      "linear-gradient(135deg, rgb(0,210,238), rgb(0,83,238))",
+      "linear-gradient(135deg, rgb(238,0,153), rgb(221,0,238))",
+      "linear-gradient(135deg, rgb(0,238,155), rgb(238,178,0))",
+    ]
+  );
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+  useMotionValueEvent(x, "change", () => {
+    console.log(gradient.get());
+  });
+  useMotionValueEvent(scrollY, "change", () => {
+    console.log(scrollY.get(), scrollYProgress.get());
+  });
   return (
-    <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin
-          dragElastic={0}
-          dragConstraints={biggerBoxRef}
-          variants={boxVariants}
-          whileHover="hover"
-          whileTap="tap"
-          whileDrag="drag"
-        />
-      </BiggerBox>
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotate, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
