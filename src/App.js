d9775005da6648d1e0d8068f18fb0e446b7446d6
kangkyeongseo <kks_big@naver.com>
2023-11-16 16:02:59 +0900

    feat: framer motion layoutId

--- src/App.js
+++ src/App.js
@@ -11,38 +11,60 @@ const Wrapper = styled(motion.div)`
   gap: 100px;
 `;
 
+const Grid = styled.div`
+  width: 50vw;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 400px;
+  height: 200px;
   display: flex;
   justify-content: center;
   align-items: center;
-  border-radius: 50px;
+  border-radius: 30px;
   background-color: rgba(255, 255, 255, 1);
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.5);
 `;
 
-const Circle = styled(motion.div)`
-  width: 100px;
-  height: 100px;
-
-  background-color: #00a5ff;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.5);
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 function App() {
-  const [isClicked, setIsClicked] = useState(false);
-  const toggleClick = () => setIsClicked((prev) => !prev);
+  const [id, setId] = useState(null);
+  const toggleClick = (id) => {
+    setId(id);
+  };
   return (
-    <Wrapper onClick={toggleClick}>
-      <Box>
-        {isClicked && (
-          <Circle layoutId="circle" style={{ borderRadius: "50%", scale: 2 }} />
+    <Wrapper>
+      <Grid>
+        {[1, 2, 3, 4].map((i) => (
+          <Box key={i} layoutId={i} onClick={() => toggleClick(i)} />
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {id && (
+          <Overlay
+            onClick={() => toggleClick(null)}
+            initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+            animate={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+            exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+          >
+            <Box layoutId={id} style={{ width: 500, height: 250 }} />
+          </Overlay>
         )}
-      </Box>
-      <Box>
-        {!isClicked && <Circle layoutId="circle" style={{ borderRadius: 0 }} />}
-      </Box>
+      </AnimatePresence>
     </Wrapper>
   );
 }
