c5812f6a6736fc91bb09c33e0c97a01057e0e7f7
kangkyeongseo <kks_big@naver.com>
2023-11-16 15:21:59 +0900

    feat: framer motion layout

--- src/App.js
+++ src/App.js
@@ -8,72 +8,41 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  gap: 100px;
 `;
 
 const Box = styled(motion.div)`
-  width: 300px;
-  height: 300px;
-  position: absolute;
-  top: 30px;
+  width: 400px;
+  height: 400px;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 28px;
   border-radius: 50px;
   background-color: rgba(255, 255, 255, 1);
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.5);
 `;
 
-const boxVariants = {
-  entry: (isBack) => ({
-    opacity: 0,
-    scale: 0,
-    x: isBack ? -300 : 300,
-    rotateZ: isBack ? -10 : 10,
-  }),
-  center: {
-    opacity: 1,
-    scale: 1,
-    x: 0,
-    rotateZ: 0,
-    transition: { duration: 0.3 },
-  },
-  exit: (isBack) => ({
-    opacity: 0,
-    scale: 0,
-    x: isBack ? 300 : -300,
-    rotateZ: isBack ? 10 : -10,
-    transition: { duration: 0.3 },
-  }),
-};
+const Circle = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+
+  background-color: #00a5ff;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.5);
+`;
 
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [isBack, setIsBack] = useState(false);
-  const nextVisible = () => {
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-    setIsBack(false);
-  };
-  const preVisible = () => {
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-    setIsBack(true);
-  };
+  const [isClicked, setIsClicked] = useState(false);
+  const toggleClick = () => setIsClicked((prev) => !prev);
   return (
-    <Wrapper>
-      <AnimatePresence mode="wait" custom={isBack}>
-        <Box
-          key={visible}
-          custom={isBack}
-          variants={boxVariants}
-          initial="entry"
-          animate="center"
-          exit="exit"
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button onClick={preVisible}>pre</button>
-      <button onClick={nextVisible}>next</button>
+    <Wrapper onClick={toggleClick}>
+      <Box>
+        {isClicked && (
+          <Circle layoutId="circle" style={{ borderRadius: "50%", scale: 2 }} />
+        )}
+      </Box>
+      <Box>
+        {!isClicked && <Circle layoutId="circle" style={{ borderRadius: 0 }} />}
+      </Box>
     </Wrapper>
   );
 }
